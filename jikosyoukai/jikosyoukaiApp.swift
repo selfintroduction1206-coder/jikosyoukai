a88eca3bdf9aaef92c39dcad1ca71c223d0539ba
MzAsh <mz.ash.ofgmail.com>
2025-12-06 01:43:00 +0900

    Initial Commit

---
+++ jikosyoukai/jikosyoukaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  jikosyoukaiApp.swift
+//  jikosyoukai
+//
+//  Created by APPLE on 2025/12/06.
+//
+
+import SwiftUI
+
+@main
+struct jikosyoukaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ jikosyoukaiTests/jikosyoukaiTests.swift
@@ -0,0 +1,17 @@
+//
+//  jikosyoukaiTests.swift
+//  jikosyoukaiTests
+//
+//  Created by APPLE on 2025/12/06.
+//
+
+import Testing
+@testable import jikosyoukai
+
+struct jikosyoukaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
